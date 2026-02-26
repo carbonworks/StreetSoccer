46d5c73b84b6565ec62f9012b3fefc13bdc4f0ba
donatello <randall@carbonworks.tech>
2026-02-26 14:15:36 -0500

    Add lifecycle overrides to AndroidLauncher for logcat debugging

--- android/src/main/kotlin/com/streetsoccer/android/AndroidLauncher.kt
+++ android/src/main/kotlin/com/streetsoccer/android/AndroidLauncher.kt
@@ -1,17 +1,39 @@
 package com.streetsoccer.android
 
 import android.os.Bundle
+import android.util.Log
 import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 import com.streetsoccer.GameBootstrapper
 
 class AndroidLauncher : AndroidApplication() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         val config = AndroidApplicationConfiguration().apply {
             useAccelerometer = false
             useCompass = false
         }
         initialize(GameBootstrapper(), config)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+        super.onDestroy()
+    }
+
+    companion object {
+        private const val TAG = "AndroidLauncher"
     }
 }
